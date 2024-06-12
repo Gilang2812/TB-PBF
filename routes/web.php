52e2b5e0528785be07fcd91e95a69c3bf3f8ebd3
Fahri Andika Sanjaya <89976566+fahri-as@users.noreply.github.com>
2024-06-12 21:24:36 +0700

    fahri ganteng bgt oii

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,7 @@ use App\Http\Controllers\BookController;
 use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\TransactionController;
 use App\Http\Controllers\PenerbitController;
+use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -41,6 +42,16 @@ Route::middleware(['auth'])->group(function () {
     Route::get('/penerbit/{id}', [PenerbitController::class, 'edit'])->name('penerbit.edit');
     Route::patch('/penerbit/{id}', [PenerbitController::class, 'update'])->name('penerbit.update');
     Route::delete('/penerbit/{id}/delete', [PenerbitController::class, 'destroy'])->name('penerbit.destroy');
+});
+
+// Rute untuk users
+Route::middleware(['auth'])->group(function () {
+    Route::get('/users', [UserController::class, 'index'])->name('users.index');
+    Route::get('/users/create', [UserController::class, 'create'])->name('users.create');
+    Route::post('/users/create', [UserController::class, 'store']);
+    Route::get('/users/{id}', [UserController::class, 'edit'])->name('users.edit');
+    Route::patch('/users/{id}', [UserController::class, 'update'])->name('users.update');
+    Route::delete('/users/{id}/delete', [UserController::class, 'destroy'])->name('users.destroy');
 });
 
 // Route for dashboard
